8fbfb45fa4c709b4f698ccb22c891780e4b2721a
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-03 12:00:11 +0100

    Add files via upload

---
+++ Schreiben/teil1/1/solutions.js
@@ -0,0 +1,33 @@
+// Structure with the answers 
+const Solutions = [
+    {
+        id: 0,
+        a: [{ text: 'Aytogmus, Lydia'}]
+    },
+    {
+        id: 1,
+        a: [{ text: '12.03.1981'},
+            { text: '12.03.81'},
+            { text: '12. März 1981'},
+            { text: '12. März 81'}]
+    },
+    {
+        id: 2,
+        a: [{ text: 'Mersin'},
+            { text: 'Türkei'},
+            { text: 'Mersin, Türkei'},
+            { text: 'Türkei, Mersin'}]
+    },
+    {
+        id: 3,
+        a: [{ text: 'Türkisch'}]
+    },
+    {
+        id: 4, 
+        a: [{ text: 'Arabisch'}]
+    },
+    {
+        id: 5,
+        a: [{ text: 'vormittags (9-12)'}]
+    }
+];
