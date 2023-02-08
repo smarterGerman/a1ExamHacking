4e7181681380a0e5061fa98753e61f52f411e9ee
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-08 15:08:30 +0100

    Add files via upload

---
+++ Schreiben/rc/teil1/script.js
@@ -0,0 +1,69 @@
+// CONSTANTS
+const container = document.querySelector('.container');
+const firstField = document.querySelector('#first');
+const secondField = document.querySelector('#second');
+const thirdField = document.querySelector('#third');
+const fourthField = document.querySelector('#fourth');
+const fifthField = document.querySelector('#fifth');
+// const sixth1Field = document.querySelector('#sixth1');
+const radio1Field = document.querySelector('.radio1');
+const radio2Field = document.querySelector('.radio2');
+const radio3Field = document.querySelector('.radio3');
+const radio1Button = document.querySelector('#radio1');
+const radio2Button = document.querySelector('#radio2');
+const radio3Button = document.querySelector('#radio3');
+const miscField = document.querySelector('.misc');
+const checkButton = document.querySelector('.check-button');
+// TODO: Make pre-filled fields uneditable 
+
+
+
+function checkInput(id, field) {
+    // correct or incorrect tracker for multiple answers 
+    let isValid = 0;
+    Solutions[id].a.forEach((element) => {
+        // console.log(field.value.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""));
+        // console.log(element.text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""));
+        if(field.value.toLowerCase().trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]\s/g,"") == element.text.trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]\s/g,"")){
+            isValid = 1;
+        }
+    })
+    if(isValid == 1){
+        field.classList.remove('incorrect');
+        field.classList.add('correct');
+        isValid = 0;
+    } else {
+        field.classList.remove('correct');
+        field.classList.add('incorrect');
+    }
+    miscField.classList.add('correct');
+}
+// Function to remove the coloring for the radio buttons 
+// function uncheckRadio() {
+//     radio1Field.classList.remove('radio-correct');
+//     radio1Field.classList.remove('radio-incorrect');
+//     radio2Field.classList.remove('radio-correct');
+//     radio2Field.classList.remove('radio-incorrect');
+//     radio3Field.classList.remove('radio-correct');
+//     radio3Field.classList.remove('radio-incorrect');
+// }
+// function checkRadio(id) {
+//     Solutions[id].a.forEach((element) => {
+//         if($('input[name="writing"]:checked').val().trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()] /g,"") == element.text.trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()] /g,"")) {
+//             uncheckRadio();
+//             $('input[name="writing"]:checked').parent()[0].classList.add('radio-correct');
+//             $('input[name="writing"]:checked').parent()[0].classList.remove('radio-incorrect')
+//         } else {
+//             uncheckRadio();
+//             $('input[name="writing"]:checked').parent()[0].classList.remove('radio-correct');
+//             $('input[name="writing"]:checked').parent()[0].classList.add('radio-incorrect')
+//         }
+//     })
+// }
+checkButton.addEventListener('click', () => {
+    checkInput(0, firstField);
+    checkInput(1, secondField);
+    checkInput(2, thirdField);
+    checkInput(3, fourthField);
+    checkInput(4, fifthField);
+})
---
+++ Schreiben/rc/teil1/solutions.js
@@ -0,0 +1,27 @@
+// Structure with the answers 
+const Solutions = [
+    {
+        id: 0,
+        a: [{ text: 'Johannes Beck'},
+            { text: 'Johannes, Beck'},
+            { text: 'Beck, Johannes'},
+            { text: 'Beck Johannes'}]
+    },
+    {
+        id: 1,
+        a: [{ text: '34'}]
+    },
+    {
+        id: 2,
+        a: [{ text: 'Chemiker'}]
+    },
+    {
+        id: 3,
+        a: [{ text: '5'}]
+    },
+    {
+        id: 4, 
+        a: [{ text: 'Segelboote'},
+            { text: 'Segelboot'}]
+    }
+];
