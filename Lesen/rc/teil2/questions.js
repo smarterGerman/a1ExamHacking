ee2e5d4a0bd7d24450e1521f8e3f51bba11535f6
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-08 12:14:10 +0100

    Add files via upload

---
+++ Lesen/rc/teil2/questions.js
@@ -0,0 +1,34 @@
+// Create data structure of questions with the correct answer highlighted 
+const title = "Reality Check | Lesen - Teil 02 "
+const Questions = [
+{
+    id: 0,
+    q: 'Sie haben Interesse an Computern und Technik und möchten eine Programmiersprache lernen.', 
+    a: [{ text: 'img/Reality Check Lesen Teil 02 - 1 a.png', isCorrect: true},
+        { text: 'img/Reality Check Lesen Teil 02 - 1 b.png', isCorrect: false}]
+},
+{
+    id: 1,
+    q: 'Sie suchen eine Wohnung für mehrere Monate auf dem Land.', 
+    a: [{ text: 'img/Reality Check Lesen Teil 02 - 2 a.png', isCorrect: false},
+        { text: 'img/Reality Check Lesen Teil 02 - 2 b.png', isCorrect: true}]
+},
+{
+    id: 2,
+    q: 'Sie möchten Gitarre spielen lernen und suchen einen Abendkurs.', 
+    a: [{ text: 'img/Reality Check Lesen Teil 02 - 3 a.png', isCorrect: false},
+        { text: 'img/Reality Check Lesen Teil 02 - 3 b.png', isCorrect: true}]
+},
+{
+    id: 3,
+    q: 'Sie möchten für den Abend einen Tisch in einem Restaurant reservieren. Wo rufen Sie an?', 
+    a: [{ text: 'img/Reality Check Lesen Teil 02 - 4 a.png', isCorrect: true},
+        { text: 'img/Reality Check Lesen Teil 02 - 4 b.png', isCorrect: false}]
+},
+{
+    id: 4,
+    q: 'Sie möchten am Samstag Nachmittag in die Sauna gehen. Wohin gehen Sie?', 
+    a: [{ text: 'img/Reality Check Lesen Teil 02 - 5 a.png', isCorrect: true},
+        { text: 'img/Reality Check Lesen Teil 02 - 5 b.png', isCorrect: false}]
+}
+];
