741d1a7647ec475ec101e9641825c4d9ace2472c
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-08 12:14:48 +0100

    Add files via upload

---
+++ Lesen/rc/teil3/questions.js
@@ -0,0 +1,34 @@
+// Create data structure of questions with the correct answer highlighted 
+const title = "Reality Check | Lesen - Teil 03 "
+const Questions = [
+{
+    id: 0,
+    q: 'Die U-Bahn Linie 5 fährt ohne Unterbrechungen bis zum Rathaus Neuburg.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 1,
+    q: 'Heute gibt es kein Popcorn im Kino.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+},
+{
+    id: 2,
+    q: 'Es gibt keine Tickets mehr für das Konzert.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 3,
+    q: 'Man darf den ganzen Tag lang keine laute Musik spielen.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 4,
+    q: 'Sie könnnen auch zur Party kommen.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+}
+];
---
+++ Lesen/rc/teil3/script.js
@@ -0,0 +1,104 @@
+// Delay function 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function sleepFor(seconds, whattodo){
+    for (let i = 0; i < seconds; i++){
+        await sleep(i*1000);
+    }
+    whattodo();
+}
+
+
+// Collect all the necessary containers 
+const questionContainer = document.querySelector('.question');
+const optionsContainer = document.querySelector('right-wrong-options');
+const gameScreen = document.querySelector('.game-screen');
+const finishScreen = document.querySelector('.finish-screen');
+const rightButton = document.querySelector('.right');
+const wrongButton = document.querySelector('.wrong');
+const scoreMessage = document.querySelector('.score');
+const titleContainer = document.querySelector('.title')
+const imgContainer = document.querySelector('.img-src');
+
+// Necessary variables for points and other values 
+let ncorrect = 0;
+let baseButtonColor = "#D7EBFA";
+let buttonClickedColor = "#A6BFDC";
+let rightColor = "#23CD9C";
+let wrongColor = "#FF8080";
+let start = true;
+let id = 0;
+
+
+
+rightButton.addEventListener('click', () => {
+    rightButton.style.backgroundColor = buttonClickedColor;
+})
+
+// Iterate through all the questions 
+function iterate(id) {
+    wrongButton.removeAttribute('style');
+    wrongButton.classList.remove('unclickable');
+    rightButton.removeAttribute('style');
+    rightButton.classList.remove('unclickable');
+    // Put the current question into the question div
+    questionContainer.innerText = Questions[id].q;
+
+    // Create a copy of the right and wrong buttons to give them the true/false values
+    imgContainer.src = "img/Reality Check - Lesen Teil 03 - " + (id + 1) + ".png";
+
+    rightButton.value = Questions[id].a[0].isCorrect;
+    wrongButton.value = Questions[id].a[1].isCorrect;
+    titleContainer.innerText = title + "(" + (id + 1) + "/" + Questions.length + ")";
+    // if(rightButton.value) {
+    //     console.log("The answer you picked is correct.")
+    // }    
+    // Set start variable to false
+    start = false;
+}
+// Retake Function 
+function retakeQuiz(){
+    finishScreen.classList.add('hide');
+    gameScreen.classList.remove('hide');
+    id = 0;
+    ncorrect = 0;
+    iterate(0);
+}
+// Create event listener for both buttons
+document.querySelectorAll('button').forEach(item => {
+    item.addEventListener('click', event => {
+        // Make the right and wrong buttons unclickable
+        wrongButton.classList.add('unclickable');
+        rightButton.classList.add('unclickable');
+        console.log(item.value);
+        if(item.value == "true"){
+            ncorrect++;
+            sleepFor(1, () => {
+                console.log(item.value);
+                item.style.backgroundColor = rightColor;
+            })
+        } else {
+            sleepFor(1, () => {
+                item.style.backgroundColor = wrongColor;
+            })
+        }
+        sleepFor(2, () => {
+            if(id < Questions.length - 1){
+                id++;
+                item.style.backgroundColor = baseButtonColor;
+                wrongButton.classList.remove('unclickable');
+                rightButton.classList.remove('unclickable');
+                iterate(id);
+            } else {
+                console.log("In the finish screen");
+                gameScreen.classList.add('hide');
+                finishScreen.classList.remove('hide');
+                scoreMessage.innerText = ncorrect + "/" + Questions.length;
+            }
+        })
+    })
+})
+
+
+initialize: if(start) iterate(0);
