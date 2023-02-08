eaf05cff85aea33e58a798d5c427df94c81e1b15
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-08 11:28:50 +0100

    Add files via upload

---
+++ Hören/rc/teil2/questions.js
@@ -0,0 +1,28 @@
+// Create data structure of questions with the correct answer highlighted 
+const title = "Reality Check | Hören - Teil 02 "
+const Questions = [
+{
+    id: 0,
+    q: 'Der Treffpunkt ist vor dem Kino.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+},
+{
+    id: 1,
+    q: 'Der zug hält nicht an der Haltestelle Hauptstraße.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 2,
+    q: 'Für Fragen zu Bestellungen muss man 1 drücken.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+},
+{
+    id: 3,
+    q: 'Herr Müller kommt am Samstag zurück.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+}
+];
