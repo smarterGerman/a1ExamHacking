60092dc459531d6234463c42c438c166a02f8d6d
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-02-08 12:13:27 +0100

    Add files via upload

---
+++ Lesen/rc/teil1/questions.js
@@ -0,0 +1,34 @@
+// Create data structure of questions with the correct answer highlighted 
+const title = "Reality Check | Lesen - Teil 01 "
+const Questions = [
+{
+    id: 0,
+    q: 'Chris soll Lena das Ticket am Bahnhof um 9:10 geben.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 1,
+    q: 'Lena fährt morgen mit Chris nach München.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+},
+{
+    id: 2,
+    q: 'Margareth singt in einem Singer-Songwriter Konzert.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+},
+{
+    id: 3,
+    q: 'Das Konzert beginnt um 19:00.', 
+    a: [{ text: 'Richtig', isCorrect: true},
+        { text: 'Falsch', isCorrect: false}]
+},
+{
+    id: 4,
+    q: 'Es gibt ein großes Buffet mit vielen Speisen und Getränken im Theater.', 
+    a: [{ text: 'Richtig', isCorrect: false},
+        { text: 'Falsch', isCorrect: true}]
+}
+];
