8e51c9e0a59b98e2aef505408942ba78d10cd23f
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-03-02 18:42:03 +0100

    Add files via upload

---
+++ Sprechen/teil1/triggers.js
@@ -0,0 +1,16 @@
+const triggers = ["Name?", "Alter?", "Land?", "Wohnort?", "Sprachen?", "Beruf?", "Hobby?", "Wie ist Ihr Familienname?", "Können Sie das buchstabieren?","Haben Sie ein Handy?","Wie ist bitte Ihre Nummer?"];
+
+
+let allTriggerAnswersJSON = `[
+    {"Name?" : ["Wie heißt du?", "Ich heiße", "Mein Name ist"],
+    "Alter?" : ["Wie alt bist du?", "Ich bin"],
+    "Land?" : ["Woher kommst du?", "Ich komme aus", "Ich bin aus", "Mein Land heißt", "Meine Heimat ist"],
+    "Wohnort?" : ["Wo wohnst du?", "Ich wohne in", "Ich lebe in"],
+    "Sprachen?" : ["Welche Sprachen sprichst du?", "Ich spreche"],
+    "Beruf?" : ["Was ist dein Beruf?", "Ich bin", "Ich arbeite als"],
+    "Hobby?" : ["Was sind deine Hobbies?", ""],
+    "Wie ist Ihr Familienname?" : ["Wie ist Ihr Familienname?","Ich heiße", "Mein Familienname ist", "Mein Name ist"],
+    "Können Sie das buchstabieren?" : ["Können Sie das buchstabieren?", ""],
+    "Haben Sie ein Handy?" : ["Haben Sie ein Handy?","Ja", "Ja, habe ich.", "Ja, ich habe ein Handy."],
+    "Wie ist bitte Ihre Nummer?" : ["Wie ist bitte Ihre Nummer?","0", "1", "2", "3", "4", "5", "6","7","8","9"]}
+    ]`;
